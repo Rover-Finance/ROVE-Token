8213ee1a5a9306c0583a7d211b70e8984c61df40
sounxk <sounakghosh.official@gmail.com>
2023-09-23 02:25:33 +0530

    Update RoverToken.sol

--- RoverToken.sol
+++ RoverToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.15;
+pragma solidity 0.8.15;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -11,27 +11,27 @@ import "@openzeppelin/contracts/security/Pausable.sol";
  */
 contract RoverToken is ERC20, Ownable, Pausable {
     // Tokenomics parameters
-    uint256 public constant totalSupplyCap = 100000000 * 10**18; // 100 million ROVE tokens cap
+    uint256 private constant totalSupplyCap = 1e8 * 10**18; // 100 million ROVE tokens cap
 
     /**
      * @dev Constructor to initialize the ROVE token.
      */
     constructor() ERC20("ROVER TOKEN", "ROVE") {
         // Mint the initial total supply to the contract owner
-        _mint(msg.sender, totalSupplyCap);
+        _mint(msg.sender, (totalSupplyCap / 10) * 8); // 80% of total supply cap
     }
 
     /**
      * @dev Pause all token functionalities (onlyOwner).
      */
-    function pause() public onlyOwner {
+    function pause() external onlyOwner {
         _pause();
     }
 
     /**
      * @dev Unpause all token functionalities (onlyOwner).
      */
-    function unpause() public onlyOwner {
+    function unpause() external onlyOwner {
         _unpause();
     }
 
@@ -40,11 +40,34 @@ contract RoverToken is ERC20, Ownable, Pausable {
      * @param account The address to mint tokens to.
      * @param amount The amount of tokens to mint.
      */
-    function mint(address account, uint256 amount) public onlyOwner {
+    function mint(address account, uint256 amount) external onlyOwner {
         require(account != address(0), "Invalid address");
-        require(amount > 0, "Amount must be greater than 0");
+        require(amount != 0, "Amount must be greater than 0");
         require(totalSupply() + amount <= totalSupplyCap, "Exceeds supply cap");
 
         _mint(account, amount);
+    }
+
+    /**
+     * @dev Burn tokens (onlyOwner).
+     * @param account The address to burn tokens from.
+     * @param amount The amount of tokens to burn.
+     */
+    function burn(address account, uint256 amount) external onlyOwner {
+        require(account != address(0), "Invalid address");
+        require(amount != 0, "Amount must be greater than 0");
+
+        _burn(account, amount);
+    }
+
+    /**
+     * @dev Override the internal function _beforeTokenTransfer from Openzeppelin’s ERC20 contract and add the whenNotPaused modifier to the function.
+     */
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override whenNotPaused {
+        super._beforeTokenTransfer(from, to, amount);
     }
 }
